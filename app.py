f0f9e0abefbf1b4aaa31ca5a80a1b27c769bab38
mikey <mikey@hey.com>
2023-12-06 17:03:16 -0600

    add delete route

--- app.py
+++ app.py
@@ -23,8 +23,14 @@ todos = db.todos
 def index():
     if request.method=='POST':
         content = request.form['content']
-        priority = request.form['priority']
-        todos.insert_one({'content': content, 'priority': priority})
+        degree = request.form['priority']
+        todos.insert_one({'content': content, 'priority': degree})
         return redirect(url_for('index'))
 
-    return render_template('index.html')
+    all_todos = todos.find().sort('content')
+    return render_template('index.html', todos=all_todos)
+
+@app.post('/<id>/delete/')
+def delete(id):
+    todos.delete_one({"_id": ObjectId(id)})
+    return redirect(url_for('index'))
